c7a72ef2b9254b0e307011ce52da3de4c9fae9a6
Arie <arie@wego.com>
2013-08-28 17:38:40 +0700

    Initial Commit

---
+++ buildApps/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  buildApps
+//
+//  Created by Arie on 8/28/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "netraAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([netraAppDelegate class]));
+	}
+}
---
+++ buildApps/netraAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  netraAppDelegate.h
+//  buildApps
+//
+//  Created by Arie on 8/28/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface netraAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
